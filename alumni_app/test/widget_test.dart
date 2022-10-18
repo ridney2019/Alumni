3f5e0355fc0d5c2c11700ddf7c15a47982626596
ridney2019 <ridneygbs@gmail.com>
2022-10-18 11:09:10 -0300

    cenários de testes

--- alumni_app/test/widget_test.dart
+++ alumni_app/test/widget_test.dart
@@ -1,44 +1,42 @@
-import 'package:alumni_app/main.dart';
+import 'package:alumni_app/views/user_form.dart';
 import 'package:alumni_app/views/user_list.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 
-// //reuso do código !
-// Widget makeTestWidget(Widget widget) => MaterialApp(
-//       home: widget,
-//     );
-
 void main() {
   group('UserList', () {
-    testWidgets('Não deve rolar com menos itens', (WidgetTester tester) async {
+    testWidgets('Não deve rolar formulário', (WidgetTester tester) async {
       await tester.pumpWidget(const MaterialApp(
-        home: UserList(),
+        home: UserForm(),
       ));
-
-      await tester.drag(find.byType(ListView), const Offset(0, -300));
+      await tester.drag(find.byType(Form), const Offset(0, -300));
       await tester.pump();
-      final primeiroUser = find.text('Carlos');
-      expect(primeiroUser, findsOneWidget);
+      // final primeiroUser = find.text('Carlos');
+      // expect(primeiroUser, findsOneWidget);
     });
 
-    testWidgets('Teste tela lista de usuários', (WidgetTester tester) async {
-      await tester.pumpWidget(const MaterialApp(
-        home: UserList(),
-      ));
-      expect(find.byType(IconButton), findsOneWidget);
-      await tester.pump();
+    testWidgets('Ecrã menor de tela', (WidgetTester tester) async {
+      tester.binding.window.physicalSizeTestValue = const Size(320, 350);
 
-      // tester.binding.window.physicalSizeTestValue = const Size(320, 350);
+      await tester.pumpWidget(
+        (const MaterialApp(
+          home: UserForm(),
+        )),
+      );
+    });
+
+    testWidgets('Deve exibir erro se a lista não for fornecida',
+        (WidgetTester tester) async {
+      await tester.pumpWidget((const MaterialApp(
+        home: UserList(),
+      )));
+      expect(find.byType(ListView), findsOneWidget);
     });
   });
 }
 
-
-
-      // expect(find.byType(ThemeData), findsOneWidget);
-      // expect(find.(ListView), findsOneWidget);
-      // final test = find.text('qualquer coisa');
-      // expect(test, findsOneWidget);
-      // await tester.tap(test);
-
-    
+// expect(find.byType(ThemeData), findsOneWidget);
+// expect(find.(ListView), findsOneWidget);
+// final test = find.text('qualquer coisa');
+// expect(test, findsOneWidget);
+// await tester.tap(test);
